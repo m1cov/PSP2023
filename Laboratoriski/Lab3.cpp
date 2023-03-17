a8e086c429d5ad882780eba708f9bb9c4fd991c8
Rade Micov <80459494+m1cov@users.noreply.github.com>
2023-03-17 18:55:17 +0100

    Lab 3

---
+++ Laboratoriski/Lab3.cpp
@@ -0,0 +1,124 @@
+#include <iostream>
+
+using namespace std;
+
+const int STACKSIZE = 10;
+typedef int info_t;
+
+struct stack
+{
+
+    info_t broj[STACKSIZE];
+    int top;
+
+    void init()
+    {
+
+        top = -1;
+    }
+
+    void push(info_t elem)
+    {
+
+        if (top >= STACKSIZE - 1)
+        {
+
+            cout << "STACKOVERFLOW";
+            exit(-1);
+        }
+
+        top++;
+        broj[top] = elem;
+    }
+
+    info_t peek()
+    {
+
+        if (top < 0)
+        {
+
+            cout << "STACKUNDERFLOW";
+            exit(-1);
+        }
+
+        return broj[top];
+    }
+
+    info_t pop()
+    {
+
+        if (top < 0)
+        {
+
+            cout << "STACKUNDERFLOW";
+            exit(-1);
+        }
+
+        top--;
+        return broj[top + 1];
+    }
+
+    bool isEmpty()
+    {
+
+        return top < 0;
+    }
+
+    bool isFull()
+    {
+
+        return top >= STACKSIZE - 1;
+    }
+};
+
+void func(stack &m)
+{
+
+    stack rez, obr;
+
+    rez.init();
+    obr.init();
+
+    while (!m.isEmpty())
+    {
+
+        obr.push(m.peek());
+        rez.push(m.pop());
+    }
+
+    while (!rez.isEmpty())
+    {
+        m.push(rez.pop());
+    }
+
+    while (!m.isEmpty())
+    {
+        rez.push(m.pop() + obr.pop());
+    }
+
+    while (!rez.isEmpty())
+    {
+        cout << rez.pop();
+    }
+}
+
+int main()
+{
+
+    stack s;
+    int n, element, c;
+    s.init();
+    cout << "Vnesete broj koj ke se proveri: " << endl;
+    cin >> n;
+
+    while (n != 0)
+    {
+        element = n % 10;
+        n /= 10;
+        s.push(element);
+    }
+
+    func(s);
+
+    return 0;
+}
