3e4ba7f3e7432c93596c3b066eb003e15f5f9235
Rade Micov <80459494+m1cov@users.noreply.github.com>
2023-03-26 15:29:25 +0200

    udpate

--- Laboratoriski/Lab 2 Grupi/cet.cpp
+++ Laboratoriski/Lab 2 Grupi/cet.cpp
@@ -25,12 +25,26 @@ struct apteka
     }
 };
 
+apteka defaultApteka[] = {{"Why", 1, true}};
+
 struct lanec
 {
 
     string lanecime;
     int brapteki;
     apteka apteki[10];
+
+    void init(string ime = "Marce", int br = 1, apteka *ap = defaultApteka)
+    {
+
+        lanecime = ime;
+        brapteki = br;
+
+        for (int i = 0; i < br; i++)
+        {
+            apteki[i] = ap[i];
+        }
+    }
 
     void pechati()
     {
@@ -58,10 +72,62 @@ struct lanec
 
 void najGolemaApteka(lanec niza[], int dolzina)
 {
+    int najgolema = 0, pozl, poza;
+    int i, j;
+
+    for (i = 0; i < dolzina; i++)
+    {
+
+        int sum = 0;
+
+        for (j = 0; j < niza[i].brapteki; j++)
+        {
+
+            if (niza[i].apteki[j].raboti == true)
+                sum += 5;
+            else
+                sum += niza[i].apteki[j].vraboteni;
+        }
+
+        if (sum > najgolema)
+        {
+
+            najgolema = sum;
+            pozl = i;
+            poza = j;
+        }
+    }
+
+    niza[pozl].apteki[poza].pechati();
 }
 
 int main()
 {
+
+    lanec lanci[2];
+    apteka apteki[10];
+    int brapteki, vraboteni;
+    bool raboti;
+    string lok, ime;
+
+    for (int i = 0; i < 2; i++)
+    {
+        cin >> ime >> brapteki;
+
+        for (int i = 0; i < brapteki; i++)
+        {
+            cin >> lok >> vraboteni >> raboti;
+
+            apteki[i].init(lok, vraboteni, raboti);
+        }
+
+        lanci[i].init(ime, brapteki, apteki);
+    }
+
+    for (int i = 0; i < 2; i++)
+        lanci[i].pechati(true);
+
+    najGolemaApteka(lanci, 2);
 
     return 0;
 }
