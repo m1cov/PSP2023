cf5b5e87c4cace10dca067e3af88dc1b087f924e
Rade Micov <80459494+m1cov@users.noreply.github.com>
2023-04-24 20:19:34 +0200

    water is wet

--- Laboratoriski/Lab7.cpp
+++ Laboratoriski/Lab7.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-class kosarkar
+class Kosarkar
 {
 
 private:
@@ -10,24 +10,92 @@ private:
     int plata;
 
 public:
-    kosarkar();
-    kosarkar(string i, int p);
+    Kosarkar(string i = "", int p = 30000)
+    {
+
+        ime = i;
+        plata = p;
+    }
+
+    void pechati()
+    {
+
+        cout << ime << " " << plata << endl;
+    }
+
+    void setIme(string i = "")
+    {
+
+        ime = i;
+    }
+
+    void presmetaj(float prosek)
+    {
+
+        while ((int)prosek > 0)
+        {
+
+            plata += 50;
+            prosek -= 1;
+        }
+    }
 };
 
-class ekipa
+class Ekipa
 {
 
 private:
     string ekipaime;
     int br;
     float prosek;
-    kosarkar list[8];
+    Kosarkar list[8];
 
 public:
+    Ekipa(string ei = "", int n = 0, float p = 0, Kosarkar *k = NULL)
+    {
+
+        ekipaime = ei;
+        br = n > 0 ? n : 0;
+        prosek = p > 0.0 ? p : 0.0;
+
+        for (int i = 0; i < br; i++)
+        {
+            list[i] = k[i];
+        }
+    }
+
+    void pechati()
+    {
+
+        cout << ekipaime << " " << prosek << " " << br << endl;
+
+        for (int i = 0; i < br; i++)
+        {
+            list[i].pechati();
+        }
+    }
 };
 
 int main()
 {
 
-    kosarkar k[5];
+    Kosarkar k[5];
+
+    string ime;
+
+    for (int i = 0; i < 5; i++)
+    {
+
+        cin >> ime;
+        k[i].setIme(ime);
+    }
+
+    for (int i = 0; i < 5; i++)
+    {
+        k[i].presmetaj(85);
+    }
+
+    Ekipa e("Insert Kosarkarska Ekipa", 5, 85, k);
+
+    e.pechati();
 }
