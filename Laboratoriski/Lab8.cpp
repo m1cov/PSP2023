8c26812ee09ce815d1ecd89545031c83a967f5c3
Matroix123 <rade.micov@gmail.com>
2023-05-02 07:52:25 +0200

    vajda raboti

--- Laboratoriski/Lab8.cpp
+++ Laboratoriski/Lab8.cpp
@@ -24,6 +24,14 @@ public:
         }
     }
 
+    Fakultet(const Fakultet &from){
+
+        ime = from.ime;
+        predmeti = from.predmeti;
+        krediti = from.krediti;
+
+    }
+
     int brojStandardniPredmeti()
     {
 
@@ -40,6 +48,10 @@ public:
 
     void pechati()
     {
+
+        cout << ime << " " << predmeti << endl;
+        for (int i = 0; i < predmeti; i++)
+            cout << krediti[i];
     }
 };
 
@@ -52,16 +64,42 @@ private:
     int br;
 
 public:
-    Univerzitet()
+    Univerzitet(string i = "", int n = 0, Fakultet *f = NULL)
     {
+
+        ime = i;
+        br = n >= 0 ? n : 0;
+
+        for (int i = 0; i < br; i++)
+            list[i] = f[i];
+
+    }
+
+    Univerzitet(const Univerzitet &from){
+
+        ime = from.ime;
+        br = from.br;
+        list = from.list;
+
     }
 
     Fakultet najmnoguSo6Krediti()
     {
+        int najmnogu = 0, poz = 0;
+
+
+        for (int i = 0; i < br; i++)
+        {
+            if(list[i].brojStandardniPredmeti() > najmnogu){
+                poz = i;
+                najmnogu = list[i].brojStandardniPredmeti();
+            }
+        }
+        
+        return list[poz];
+
     }
 };
-
-// TODO: za dvete dodadi Copy Constructor
 
 int main()
 {
@@ -69,7 +107,30 @@ int main()
     Fakultet list[2];
     Univerzitet uni;
 
-    // TODO: Dodadi za vnes na faks
+    string imeu, imef;
+    int *x = new int[0], predmeti;
+
+    cout << "Vnesi ime na univerzitetot" << endl;
+    cin >> imeu;
+
+    for (int i = 0; i < 2; i++)
+    {
+        cout << "Vnesi ime na fakultetot" << endl;
+        cin >> imef;
+        cout << "Vnesi kolku predmeti ima na fakultetot" << endl;
+        cin >> predmeti;
+        
+        x = new int[predmeti];
+
+        for (int i = 0; i < predmeti; i++)
+        {
+                cout << "Vnesi kolku krediti nosi predmetot" << endl;
+                cin >> x[i];
+        }
+        list[i] = Fakultet(predmeti, x, imef);
+    }
+    
+    uni = Univerzitet(imeu, 2, list);
 
     uni.najmnoguSo6Krediti().pechati();
 }
