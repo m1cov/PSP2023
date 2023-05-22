f37ff52039aea3219ce57653c7acbd71870dddf6
Matroix123 <rade.micov@gmail.com>
2023-05-22 12:42:45 +0200

    hwda

--- Nasleduvanje/z5.cpp
+++ Nasleduvanje/z5.cpp
@@ -17,6 +17,10 @@ public:
         prezime = p;
         god = g;
     }
+
+    int get_god() { return god; }
+    string getIme() { return ime; }
+    string getPrezime() { return prezime; }
 };
 
 class drustvo
@@ -53,6 +57,106 @@ public:
         for (int i = 0; i < n; i++)
         {
             list[i] = d.list[i];
+        }
+    }
+
+    int max_god_iskustvo()
+    {
+
+        int g = list[0].get_god();
+
+        for (int i = 0; i < n; i++)
+        {
+            if (list[i].get_god() > g)
+                g = list[i].get_god();
+        }
+
+        return g;
+    }
+
+    drustvo &operator+=(izvidnik &t)
+    {
+
+        izvidnik *pom = new izvidnik[n + 1];
+
+        for (int i = 0; i < n; i++)
+        {
+            pom[i] = list[i];
+        }
+
+        pom[n++] = t;
+
+        delete[] list;
+        list = new izvidnik[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            list[i] = pom[i];
+        }
+
+        delete[] pom;
+    }
+
+    friend drustvo &operator+(const drustvo &a, const drustvo &b)
+    {
+
+        drustvo d;
+
+        for (int i = 0; i < a.n; i++)
+        {
+            d += a.list[i];
+        }
+
+        for (int i = 0; i < b.n; i++)
+        {
+            d += b.list[i];
+        }
+
+        return d;
+    }
+
+    drustvo &operator--()
+    {
+        int mini = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (list[i].get_god() < list[mini].get_god())
+            {
+
+                mini = i;
+            }
+        }
+
+        izvidnik *pom = new izvidnik[n - 1];
+        int j = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (i != mini)
+                pom[j++] = list[i];
+        }
+
+        delete[] list;
+        list = new izvidnik[--n];
+
+        for (int i = 0; i < n; i++)
+        {
+            list[i] = pom[i];
+        }
+
+        delete[] pom;
+
+        return *this;
+    }
+
+    friend ostream &operator<<(ostream &o, const drustvo &p)
+    {
+
+        o << p.ime << p.n << p.oblast << endl;
+
+        for (int i = 0; i < p.n; i++)
+        {
+            o << p.list[i].get_god() << p.list[i].getIme() << p.list[i].getPrezime() << endl;
         }
     }
 };
